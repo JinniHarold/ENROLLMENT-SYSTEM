135cde79f6b2bb70d176afe34f642d714c6cec97
Gene Harold <geneisme20@gmail.com>
2024-09-23 17:07:34 +0800

    Add project files.

---
+++ ENROLLMENT SYSTEM/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ENROLLMENT_SYSTEM.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult StudentInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ ENROLLMENT SYSTEM/Models/Entities/Students.cs
@@ -0,0 +1,15 @@
+﻿namespace ENROLLMENT_SYSTEM.Models.Entities
+{
+    public class Students
+    {
+        public int StudID { get; set; }
+        public string StudLastName { get; set; }
+        public string StudFirstName { get; set; }
+        public string StudMidName { get; set; }
+        public string StudCourse { get; set; }
+        public int StudYear { get; set; }
+        public string Studremarks { get; set; }
+        public string StudStatus { get; set; }
+
+    }
+}
