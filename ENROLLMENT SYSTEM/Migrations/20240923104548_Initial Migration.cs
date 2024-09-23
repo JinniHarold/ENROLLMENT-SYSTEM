567185d4e56feeb9cb6157d767150d8048967585
Gene Harold <geneisme20@gmail.com>
2024-09-23 18:48:20 +0800

    Database Connection done

---
+++ ENROLLMENT SYSTEM/Migrations/20240923104548_Initial Migration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ENROLLMENT_SYSTEM.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentEntry",
+                columns: table => new
+                {
+                    StudID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudLastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudFirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudMidName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudCourse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudYear = table.Column<int>(type: "int", nullable: false),
+                    Studremarks = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentEntry", x => x.StudID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentEntry");
+        }
+    }
+}
--- ENROLLMENT SYSTEM/Models/Entities/Students.cs
+++ ENROLLMENT SYSTEM/Models/Entities/Students.cs
@@ -1,8 +1,12 @@
-﻿namespace ENROLLMENT_SYSTEM.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ENROLLMENT_SYSTEM.Models.Entities
 {
     public class Students
     {
+        [Key]
         public int StudID { get; set; }
+
         public string StudLastName { get; set; }
         public string StudFirstName { get; set; }
         public string StudMidName { get; set; }
